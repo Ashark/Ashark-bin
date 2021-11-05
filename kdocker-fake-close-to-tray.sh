9b9c68c413d80f4f78dd60aa4dc37da9737b5555
Andrew Shark <ashark@linuxcomp.ru>
2021-11-05 21:42:39 +0300

    support only one watcher instance

--- kdocker-fake-close-to-tray.sh
+++ kdocker-fake-close-to-tray.sh
@@ -22,18 +22,52 @@
 
 app_name="$1"
 
-QT_QPA_PLATFORM=xcb GDK_BACKEND=x11 kdocker -m "$app_name"
-
-while true; do
-    ps --no-headers -C "kdocker $app_name" -o args,state
-    exit_code=$?
-    if [[ $exit_code != 0 ]]; then
-        kdialog --title "kdocker fake close to tray" --passivepopup "closed, relaunching it" 5
-        echo "The app was closed, relaunching minimized to tray"
-        # TODO Here need to create and apply a window rule for the app window. The rule is "minimized - apply initialy". So when app is relaunch, it is not shown for a second before traying.
-        QT_QPA_PLATFORM=xcb GDK_BACKEND=x11 kdocker "$app_name"
+# We want to allow only one instance of fake-ctt per app
+LOCKDIR=/tmp/fake-ctt_$app_name
+#Remove the lock directory
+function cleanup {
+    if rmdir $LOCKDIR; then
+        echo "Finished"
     else
-        echo "something went wrong"
-        sleep 5
+        echo "Failed to remove lock directory '$LOCKDIR'"
+        sleep 10
+        exit 1
     fi
-done
+}
+
+if mkdir $LOCKDIR; then
+    #Ensure that if we "grabbed a lock", we release it
+    #Works for SIGTERM and SIGINT(Ctrl-C)
+    trap "cleanup" EXIT
+
+    echo "Acquired lock, running"
+
+    keep_window_showing="-m"
+
+    while true; do
+        ps --no-headers -C "kdocker $app_name" -o args,state
+        exit_code=$?
+        if [[ $exit_code != 0 ]]; then
+            if [[ "$keep_window_showing" == "" ]]; then
+                kdialog --title "kdocker fake close to tray $$" --passivepopup "The app was closed, relaunching minimized to tray" 5
+                echo "The app was closed, relaunching minimized to tray"
+            else
+                echo "Launching first time, keeping window shown"
+            fi
+            # TODO Here need to create and apply a window rule for the app window. The rule is "minimized - apply initialy". So when app is relaunch, it is not shown for a second before traying.
+            QT_QPA_PLATFORM=xcb GDK_BACKEND=x11 kdocker $keep_window_showing "$app_name"
+            keep_window_showing=""
+        else
+            echo "something went wrong"
+            echo "probably undocked instance of app exists, killing it."
+            killall "$app_name"
+            sleep 1
+#             exit 1
+        fi
+    done
+
+else
+    echo "Could not create lock directory '$LOCKDIR'"
+    sleep 3
+    exit 1
+fi
